ef6f5db4f89df96fcb080f3e5a00d1ac16be5990
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2022-10-23 00:20:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+export default {
+  data: () => ({
+    slides: [
+      'https://picsum.photos/id/1032/900/400',
+      'https://picsum.photos/id/1033/900/400',
+      'https://picsum.photos/id/1035/900/400',
+      'https://picsum.photos/id/1036/900/400',
+      'https://picsum.photos/id/1037/900/400',
+    ]
+  })
+}
+</script>
+
+<template>
+  <div class="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item" v-for="slide in slides">
+        <img :src="slide" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
