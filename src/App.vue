2eb4359cf0afb3117f223991cf703b832d5420a8
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2022-10-23 00:58:55 +0100

    Componentized carousel

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Carousel from './components/carousel/Carousel.vue'
 export default {
   data: () => ({
     slides: [
-      'https://picsum.photos/id/1032/900/400',
-      'https://picsum.photos/id/1033/900/400',
-      'https://picsum.photos/id/1035/900/400',
-      'https://picsum.photos/id/1036/900/400',
-      'https://picsum.photos/id/1037/900/400',
+      'https://image-placeholder.com/images/actual-size/600x400.png',
+      'https://image-placeholder.com/images/actual-size/600x400.png',
+      'https://image-placeholder.com/images/actual-size/600x400.png',
+      'https://image-placeholder.com/images/actual-size/600x400.png',
+      'https://image-placeholder.com/images/actual-size/600x400.png',
     ]
-  })
+  }),
+  components: {Carousel}
 }
 </script>
 
 <template>
-  <div class="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item" v-for="slide in slides">
-        <img :src="slide" alt="">
-      </div>
-    </div>
-  </div>
+  <carousel :slides="slides" ></carousel>e
 </template>
 
 <style scoped>
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="carousel">
+        <div class="carousel-inner">
+            <caruosel-item v-for="(slide, index) in slides" 
+            :slide="slide" 
+            :key="`slide-${index}`"></caruosel-item>
+        </div>
+  </div>
+</template>
+
+<script>
+    import CaruoselItem from './CarouselItem.vue'
+
+    export default {
+        props: ['slides'],
+        components: { CaruoselItem }
+    }
+</script>
